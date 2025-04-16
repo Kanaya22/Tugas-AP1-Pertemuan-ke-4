6bad7af6571180b9a91d54acc56ccbcb63fe2d01
Kanaya22 <76435658+Kanaya22@users.noreply.github.com>
2025-04-16 10:32:43 +0700

    Add files via upload

---
+++ Tugas AP1 Pertemuan 4 Kanaya pratiwi.cpp
@@ -0,0 +1,27 @@
+#include<iostream> 
+#include <string>
+using  namespace std;
+  int main()  {
+  	string nim, nama, kelas, hobi;
+    
+    cout << "Masukkan nim: " << endl;
+    cin >> nim;
+
+    cout << "Masukkan nama: " << endl;
+    cin >> nama; 
+    
+    cout << "Masukkan kelas: " << endl;
+    cin >> kelas;
+    
+    cout << "Masukkan hobi: " << endl;
+    cin >> hobi;
+
+    cout << "== Data Diri ==" << endl;
+    cout << "NIM  : " << nim << endl;
+    cout << "Nama : " << nama << endl;
+    cout << "Kelas :" << kelas << endl;
+    cout << "Hobi :" << hobi << endl;
+    
+    return 0;
+  	
+}
